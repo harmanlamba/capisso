bfe9a943e641abb063b610d4d38e98950c209e22
Harman Lamba <harrygth15478@gmail.com>
2020-08-31 10:26:29 +1200

    NP761-1: Add entities for Organisation and Repository Pattern

---
+++ server/src/Capisso/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Capisso.Entities
+{
+    public abstract class Entity<T> : BaseEntity, IEntity<T>
+    {
+        public virtual T Id { get; set; }
+    }
+}
---
+++ server/src/Capisso/Entities/Organisation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Capisso.Entities
+{
+    public class Organisation : Entity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Classifications { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+    }
+}
