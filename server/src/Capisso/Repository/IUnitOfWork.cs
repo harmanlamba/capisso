6383e9b0b91229c4aa12894d9e21fa0ecf4a1573
Harman Lamba <harrygth15478@gmail.com>
2020-08-31 10:28:42 +1200

    NP761-1: Implement initial Unit Of Work pattern

---
+++ server/src/Capisso/Repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Capisso.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IOrganisationRepository OrganisationRepository { get; }
+        Task SaveAsync();
+    }
+}
---
+++ server/src/Capisso/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Capisso.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+        public UnitOfWork(CapissoContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            OrganisationRepository = new OrganisationRepository(_dbContext);
+        }
+
+        public IOrganisationRepository OrganisationRepository { get; private set; }
+
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
