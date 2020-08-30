5cf6bfa127fe48821a59de2c09358db8d2907f4a
Harman Lamba <harrygth15478@gmail.com>
2020-08-31 11:07:36 +1200

    NP761-1: Add test to check organisation service

---
+++ server/test/Capisso.Test/Repository/MockUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Capisso.Repository;
+using Moq;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Capisso.Test.Repository
+{
+    public class MockUnitOfWork : IUnitOfWork
+    {
+        public MockUnitOfWork(IOrganisationRepository organisationRepository)
+        {
+            OrganisationRepository = organisationRepository;
+        }
+
+        public IOrganisationRepository OrganisationRepository { get; }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Task SaveAsync()
+        {
+            return Task.Delay(0);
+        }
+    }
+}
