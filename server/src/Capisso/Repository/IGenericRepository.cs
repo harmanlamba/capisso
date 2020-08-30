852476ac87b506e2bdff6b424032f17352adf3a2
Harman Lamba <harrygth15478@gmail.com>
2020-08-31 10:25:46 +1200

    NP761-1: Implement Generic Repository Pattern

---
+++ server/src/Capisso/Repository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Capisso.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Capisso.Repository
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindByAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(object id);
+        Task InsertAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task DeleteByIdAsync(object id);
+    }
+}
