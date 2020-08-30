9a039d3f24cd7caa7cb21783964e064ab73e1396
Harman Lamba <harrygth15478@gmail.com>
2020-08-31 10:30:05 +1200

    NP761-1: Implement organisation services to add Organisation

---
+++ server/src/Capisso/Services/OrganisationService.cs
@@ -0,0 +1,26 @@
+﻿using Capisso.Dto;
+using Capisso.Mapper;
+using Capisso.Repository;
+using System.Threading.Tasks;
+
+namespace Capisso.Services
+{
+    public class OrganisationService : IOrganisationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrganisationService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> CreateOrganisation(OrganisationDto organisationDto)
+        {
+            var organisation = OrganisationMapper.FromDto(organisationDto);
+            await _unitOfWork.OrganisationRepository.InsertAsync(organisation);
+            await _unitOfWork.SaveAsync();
+
+            return organisation.Id;
+        }
+    }
+}
