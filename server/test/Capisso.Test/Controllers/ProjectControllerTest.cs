6b6c21038986cc61b2623e163db3743ebbde5c41
salma-s <ssan631@aucklanduni.ac.nz>
2020-09-03 17:36:25 +1200

    NP761-2: Implement create project tests

---
+++ server/test/Capisso.Test/Controllers/ProjectControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using Capisso.Controllers;
+using Capisso.Dto;
+using Capisso.Entities;
+using Capisso.Repository;
+using Capisso.Services;
+using Capisso.Test.Repository;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace Capisso.Test.Controllers
+{
+    public class ProjectControllertest
+    {
+        private MockUnitOfWork _mockUnitOfWork;
+        private Mock<IProjectRepository> _mockProjectRepository;
+        private ProjectService _projectService;
+        private ProjectsController _projectsController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockProjectRepository = new Mock<IProjectRepository>();
+            _mockUnitOfWork = new MockUnitOfWork
+            {
+                ProjectRepository = _mockProjectRepository.Object
+            };
+
+            _projectService = new ProjectService(_mockUnitOfWork);
+
+            _projectsController = new ProjectsController(_projectService);
+        }
+
+        [Test]
+        public async Task TestCreateProjectValidInput()
+        {
+            var projectDto = new ProjectDto
+            {
+                Title = "Title1",
+                Notes = "Note1",
+                Outcome = "Outcome1",
+                StartDate = new DateTime(),
+                EndDate = new DateTime(),
+            };
+
+            _mockProjectRepository.Setup(x => x.InsertAsync(It.IsAny<Project>())).Returns(Task.FromResult(1));
+
+            // Act
+            ActionResult<CreatedDto> response = await _projectsController.CreateProject(projectDto);
+
+            // Assert
+            Assert.IsInstanceOf<CreatedResult>(response.Result);
+            CreatedResult createdResult = response.Result as CreatedResult;
+            Assert.AreEqual(201, createdResult.StatusCode);
+
+            CreatedDto value = createdResult.Value.As<CreatedDto>();
+            Assert.IsNotNull(value);
+            Assert.AreEqual(0, value.Id);
+
+            _mockProjectRepository.Verify(x => x.InsertAsync(It.IsAny<Project>()), Times.Once);
+        }
+
+    }
+}
