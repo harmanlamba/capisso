06183a5a774905657d66428ce6b1b0c79a41f1d0
Jed Robertson <jrob928@aucklanduni.ac.nz>
2020-09-22 12:16:38 +1200

    NP761-33: Added filter box for organisations

--- client/src/pages/organisations/OrganisationsViewAllPage.tsx
+++ client/src/pages/organisations/OrganisationsViewAllPage.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { makeStyles, Box, Typography, Button } from '@material-ui/core';
+import {
+  makeStyles,
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Grid,
+} from '@material-ui/core';
 
 import { getAllOrganisations } from '../../common/api/organisations';
 import { IOrganisationDto } from '../../types/types';
@@ -17,30 +24,54 @@ export const OrganisationsViewAllPage: React.FC<{}> = () => {
   const classes = useStyles();
 
   const [organisations, setOrganisations] = useState<IOrganisationDto[]>([]);
+  const [filterTerm, setFilterTerm] = useState<string>();
 
   useEffect(() => {
     getAllOrganisations().then((data) => setOrganisations(data));
   }, []);
 
+  const filteredOrganisations = organisations.filter(
+    (organisation) =>
+      !filterTerm ||
+      organisation.name.toLowerCase().includes(filterTerm.toLowerCase()) ||
+      organisation.classifications.some((classification) =>
+        classification.toLowerCase().includes(filterTerm.toLowerCase())
+      )
+  );
+
   return (
     <div className={classes.content}>
       <Box mb={2}>
-        <Typography variant="h4" display="inline">
-          Organisations
-        </Typography>
-        <Box ml={2} position="relative" top="-0.5em" display="inline">
-          <Button
-            component={Link}
-            to="/organisations/add"
-            variant="contained"
-            color="primary"
-          >
-            + Add
-          </Button>
-        </Box>
+        <Grid
+          justify="space-between" // Add it here :)
+          container={true}
+        >
+          <Box>
+            <Typography variant="h4" display="inline">
+              Organisations
+            </Typography>
+            <Box ml={2} position="relative" top="-0.5em" display="inline">
+              <Button
+                component={Link}
+                to="/organisations/add"
+                variant="contained"
+                color="primary"
+              >
+                + Add
+              </Button>
+            </Box>
+          </Box>
+          <TextField
+            id="filter-field"
+            onChange={(event) => setFilterTerm(event.target.value)}
+            label="Filter"
+            variant="outlined"
+            size="small"
+          />
+        </Grid>
       </Box>
 
-      <OrganisationsList organisations={organisations} />
+      <OrganisationsList organisations={filteredOrganisations} />
     </div>
   );
 };
