b19aa97db19c5ecab84ecfd06ef49608e68fe7aa
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-09-22 18:36:42 +1200

    NP761-10: Add unit tests for ContactsController

---
+++ server/test/Capisso.Test/Controllers/ContactsControllerTest.cs
@@ -0,0 +1,102 @@
+using System.Threading.Tasks;
+using Capisso.Controllers;
+using Capisso.Dto;
+using Capisso.Entities;
+using Capisso.Repository;
+using Capisso.Services;
+using Capisso.Test.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace Capisso.Test.Controllers
+{
+    public class ContactsControllerTest
+    {
+        private MockUnitOfWork _mockUnitOfWork;
+        private Mock<IContactRepository> _mockContactRepository;
+        private Mock<IOrganisationRepository> _mockOrganisationRepository;
+        private ContactService _contactService;
+        private ContactsController _contactsController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockContactRepository = new Mock<IContactRepository>();
+            _mockOrganisationRepository = new Mock<IOrganisationRepository>();
+            _mockUnitOfWork = new MockUnitOfWork
+            {
+                ContactRepository = _mockContactRepository.Object,
+                OrganisationRepository = _mockOrganisationRepository.Object,
+            };
+            _contactService = new ContactService(_mockUnitOfWork);
+            _contactsController = new ContactsController(_contactService);
+        }
+
+        [Test]
+        public async Task TestCreateContactValidInput()
+        {
+            // arrange
+            var contactDto = new ContactDto
+            {
+                Name = "name",
+                PhoneNumber = "123",
+                Email = "name@name.com",
+                Notes = "notes",
+                OrganisationId = 1,
+            };
+            _mockContactRepository
+                .Setup(x => x.InsertAsync(It.IsAny<Contact>()))
+                .Returns(Task.FromResult(1))
+                .Verifiable();
+            _mockOrganisationRepository
+                .Setup(x => x.GetByIdAsync(1))
+                .ReturnsAsync(new Organisation {Id = 1})
+                .Verifiable();
+
+            // act
+            var response = await _contactsController.CreateContact(contactDto);
+
+            // assert
+            Assert.IsInstanceOf<CreatedResult>(response.Result);
+            var createdResult = response.Result as CreatedResult;
+
+            var value = createdResult.Value as CreatedDto;
+            Assert.IsNotNull(value);
+            Assert.AreEqual(0, value.Id);
+
+            _mockContactRepository.Verify();
+            _mockOrganisationRepository.Verify();
+        }
+
+        [Test]
+        public async Task TestCreateContactInvalidOrganisation()
+        {
+            // arrange
+            var contactDto = new ContactDto
+            {
+                Name = "name",
+                PhoneNumber = "123",
+                Email = "name@name.com",
+                Notes = "notes",
+                OrganisationId = 69,
+            };
+            _mockContactRepository
+                .Setup(x => x.InsertAsync(It.IsAny<Contact>()))
+                .Returns(Task.FromResult(1));
+            _mockOrganisationRepository
+                .Setup(x => x.GetByIdAsync(69))
+                .Returns(Task.FromResult<Organisation>(null))
+                .Verifiable();
+
+            // act
+            var response = await _contactsController.CreateContact(contactDto);
+
+            // assert
+            Assert.IsInstanceOf<NotFoundResult>(response.Result);
+
+            _mockContactRepository.Verify(x => x.InsertAsync(It.IsAny<Contact>()), Times.Never);
+            _mockOrganisationRepository.Verify();
+        }
+    }
+}
