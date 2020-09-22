ceb6cc2c28ff07cf19f5536ac398575773117632
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-09-22 18:29:34 +1200

    NP761-10: Rename ProjectsController file to match class name

--- server/src/Capisso/Controllers/ProjectsController.cs
+++ server/src/Capisso/Controllers/ProjectsController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Capisso.Dto;
-using Capisso.Entities;
 using Capisso.Exceptions;
 using Capisso.Services;
 using Microsoft.AspNetCore.Mvc;
