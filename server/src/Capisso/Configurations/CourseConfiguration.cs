ec40b1fca954e6f9d47578865906294e15b99d83
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-09-22 19:46:58 +1200

    NP761-8: Move entity configuration files into correct namespace

--- server/src/Capisso/Configurations/CourseConfiguration.cs
+++ server/src/Capisso/Configurations/CourseConfiguration.cs
@@ -2,30 +2,33 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CourseConfiguration : IEntityTypeConfiguration<Course>
+namespace Capisso.Configurations
 {
-    public void Configure(EntityTypeBuilder<Course> builder)
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
     {
-        builder.HasKey(e => e.Id);
-        builder.Property(e => e.Name).IsRequired();
-        builder.Property(e => e.Code).IsRequired();
-        builder.Property(e => e.Description).IsRequired();
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Name).IsRequired();
+            builder.Property(e => e.Code).IsRequired();
+            builder.Property(e => e.Description).IsRequired();
 
-        builder.HasData(
-            new Course
-            {
-                Id = 1,
-                Name = "Advanced Agile and Lean Software Development",
-                Code = "SOFTENG 761",
-                Description = "Learn how to use the agile software methodology."
-            },
-            new Course
-            {
-                Id = 2,
-                Name = "Robotic Process Automation",
-                Code = "SOFTENG 762",
-                Description = "Learn how to automate processes robitically."
-            }
-        );
+            builder.HasData(
+                new Course
+                {
+                    Id = 1,
+                    Name = "Advanced Agile and Lean Software Development",
+                    Code = "SOFTENG 761",
+                    Description = "Learn how to use the agile software methodology."
+                },
+                new Course
+                {
+                    Id = 2,
+                    Name = "Robotic Process Automation",
+                    Code = "SOFTENG 762",
+                    Description = "Learn how to automate processes robitically."
+                }
+            );
+        }
     }
 }
