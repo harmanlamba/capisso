6d9cd7d6717619f196de83c1b3d9fd78012c28d1
Harrison Leach <HarrisonL7190@gmail.com>
2020-09-24 00:37:16 +1200

    NP761-10: Remove useless escape character

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,3 +1,3 @@
 export const DRAWER_WIDTH: number = 300;
 export const EMAIL_REGEX: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-export const PHONE_NUMBER_REGEX: RegExp = /^[+]{0,1}[-\s\.()/0-9]{1,16}$/;
+export const PHONE_NUMBER_REGEX: RegExp = /^[+]{0,1}[-\s.()/0-9]{1,16}$/;
