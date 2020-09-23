3e33f83c49cf0e46c35bc53a20cf5dfb4ed72905
salma-s <ssan631@aucklanduni.ac.nz>
2020-09-23 19:50:20 +1200

    NP761-49: Add organisation link and course to project page

--- client/src/components/projects/ProjectViewAbout.tsx
+++ client/src/components/projects/ProjectViewAbout.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles, Paper, Box, Typography } from '@material-ui/core';
 import { IProjectDto } from '../../types/types';
 import moment from 'moment';
+import { useOrganisations, useCourses } from '../../common/hooks/apiHooks';
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -15,9 +18,16 @@ export const ProjectViewAbout: React.FC<{ project: IProjectDto }> = ({
   project,
 }) => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const { courses } = useCourses();
+  const { organisations } = useOrganisations();
 
   const formattedStartDate = moment(project.startDate);
   const formattedEndDate = moment(project.endDate);
+  const projectCourses = courses.filter(({ id }) =>
+    project.courseIds.includes(id!)
+  );
 
   return (
     <div className={classes.content}>
@@ -43,7 +53,6 @@ export const ProjectViewAbout: React.FC<{ project: IProjectDto }> = ({
           </Paper>
         </Box>
       )}
-
       {project.outcome && (
         <Box mb={2}>
           <Paper>
@@ -67,6 +76,51 @@ export const ProjectViewAbout: React.FC<{ project: IProjectDto }> = ({
                 {formattedStartDate.format('YYYY-MM-DD')} -{' '}
                 {formattedEndDate.format('YYYY-MM-DD')}
               </p>
+            </Box>
+          </Paper>
+        </Box>
+      )}
+      {project.organisationId && (
+        <Box mb={2}>
+          <Paper>
+            <Box p={2}>
+              <Typography variant="h6" color="primary" display="inline">
+                Organisation:
+                <Button
+                  color="primary"
+                  size="large"
+                  onClick={() =>
+                    history.push(
+                      `/organisations/${project.organisationId}/about`
+                    )
+                  }
+                >
+                  {
+                    organisations.find((o) => o.id === project.organisationId)
+                      ?.name
+                  }
+                </Button>
+              </Typography>
+            </Box>
+          </Paper>
+        </Box>
+      )}
+      {project.courseIds && (
+        <Box mb={2}>
+          <Paper>
+            <Box p={2}>
+              <Typography variant="h6" color="primary" display="inline">
+                Courses
+              </Typography>
+              <ul>
+                {projectCourses.map((course) => (
+                  <div key={course.id}>
+                    <li>
+                      {course.code}: {course.name}
+                    </li>
+                  </div>
+                ))}
+              </ul>
             </Box>
           </Paper>
         </Box>
