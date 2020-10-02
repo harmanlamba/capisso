1e0a2e0e7bbf612ff27991bd8f0e8efae7a34dad
salma-s <ssan631@aucklanduni.ac.nz>
2020-10-02 23:03:38 +1300

    NP761-45: Fix course add dialog styling

--- client/src/components/utility/AddCourseDialog.tsx
+++ client/src/components/utility/AddCourseDialog.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  makeStyles,
-} from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent } from '@material-ui/core';
 import { addCourse } from '../../common/api/courses';
 import { CoursesForm } from '../courses/CoursesForm';
-
-const useStyles = makeStyles(() => ({
-  modalContent: {
-    margin: '-50px -40px  -30px -40px',
-  },
-}));
 
 export interface IAddCourseDialogProps {
   open: boolean;
@@ -25,20 +14,16 @@ export const AddCourseDialog: React.FC<IAddCourseDialogProps> = ({
   close,
   handleSuccess,
 }) => {
-  const classes = useStyles();
-
   return (
     <Dialog open={open} onClose={close}>
       <DialogTitle>Add new course</DialogTitle>
       <DialogContent>
-        <div className={classes.modalContent}>
-          <CoursesForm
-            type="Add"
-            onSubmit={addCourse}
-            handleCancel={() => close()}
-            handleSubmitSuccess={(newCourseId) => handleSuccess(newCourseId)}
-          />
-        </div>
+        <CoursesForm
+          type="Add"
+          onSubmit={addCourse}
+          handleCancel={() => close()}
+          handleSubmitSuccess={(newCourseId) => handleSuccess(newCourseId)}
+        />
       </DialogContent>
     </Dialog>
   );
