e1f596c775fc39458fc10469a0c2cc57d51aa285
Harrison Leach <HarrisonL7190@gmail.com>
2020-10-03 13:32:51 +1300

    NP761-19: Refactor parsing to be async, still some lag issues at high volumes

--- client/src/common/hooks/parsingHooks.tsx
+++ client/src/common/hooks/parsingHooks.tsx
@@ -11,52 +11,83 @@ export interface UploadCsvUserError {
 
 export const useParseUsersCsv = (file: File | null) => {
   const [data, setData] = useState<IUserDto[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
   const [errors, setErrors] = useState<UploadCsvUserError[]>([]);
 
   useEffect(() => {
     if (file) {
+      setData([]);
       setErrors([]);
-      parse(file, {
-        complete: (res) => {
-          setLoading(false);
-          const users = res.data.map((u: any) => {
-            return { email: u.email, userRole: u.userRole || UserRole.User };
-          });
+      setLoading(true);
 
-          const validationErrors: UploadCsvUserError[] = [];
-          users.forEach((u, i) => {
-            const row = i + 1;
-            if (!u.email) {
-              validationErrors.push({ message: 'Email is required', row });
-            } else if (!EMAIL_REGEX.test(u.email)) {
-              validationErrors.push({ message: 'Invalid email address', row });
-            } else if (!UOA_EMAIL_REGEX.test(u.email)) {
-              validationErrors.push({
-                message: 'Invalid email domain (must be @aucklanduni.ac.nz)',
-                row,
-              });
-            }
-
-            if (
-              u.userRole &&
-              u.userRole !== UserRole.User &&
-              u.userRole !== UserRole.Admin
-            ) {
-              validationErrors.push({ message: 'Invalid user role', row });
-            }
-          });
-
+      parseAsync(file)
+        .then(({ users, errors: e }) => {
           setData(users);
-          setErrors(validationErrors);
-        },
-        header: true,
-        transformHeader: (_header, index) =>
-          index === 0 ? 'email' : index === 1 ? 'userRole' : '_',
-        skipEmptyLines: true,
-      });
+          setErrors(e);
+        })
+        .catch((e) => {
+          console.error(e);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, [file]);
 
   return { data, loading, errors };
+};
+
+const parseAsync = async (
+  file: File
+): Promise<{
+  users: IUserDto[];
+  errors: UploadCsvUserError[];
+}> => {
+  return new Promise((resolve) => {
+    parse(file, {
+      complete: (res) => {
+        res.data.shift(); // remove first item (the headers)
+        const users: IUserDto[] = res.data.map((u: any) => {
+          return {
+            email: u[0],
+            userRole: u[1] || UserRole.User,
+          };
+        });
+
+        resolve({ users, errors: validateUsers(users) });
+      },
+      worker: true,
+      header: false,
+      skipEmptyLines: true,
+    });
+  });
+};
+
+const validateUsers = (users: IUserDto[]): UploadCsvUserError[] => {
+  const validationErrors: UploadCsvUserError[] = [];
+  users.forEach((u, i) => {
+    const row = i + 1;
+    if (!u.email) {
+      validationErrors.push({ message: 'Email is required', row });
+    } else if (!EMAIL_REGEX.test(u.email)) {
+      validationErrors.push({
+        message: 'Invalid email address',
+        row,
+      });
+    } else if (!UOA_EMAIL_REGEX.test(u.email)) {
+      validationErrors.push({
+        message: 'Invalid email domain (must be @aucklanduni.ac.nz)',
+        row,
+      });
+    }
+
+    if (
+      u.userRole &&
+      u.userRole !== UserRole.User &&
+      u.userRole !== UserRole.Admin
+    ) {
+      validationErrors.push({ message: 'Invalid user role', row });
+    }
+  });
+  return validationErrors;
 };
